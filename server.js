f3dfd5bc459320e6c3d4d3bbfe462f0deb7bd2a8
Developer Rupangkar <developeu.rupangkar@gmail.com>
2024-02-22 19:26:06 +0600

    routes added

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 
 const express = require('express');
+const porjectRoutes = require("./routes/projectRoute");  
 
 //express app
 const app = express();
 
 //port
 const port = process.env.PORT || 4000;
+
+//middleweres
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+//routes
+app.use("/api/projects", porjectRoutes);
 
 //lestien for request 
 app.listen(port, () =>{
